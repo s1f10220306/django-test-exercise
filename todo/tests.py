bbe905b1093014e3a73fab1bed7612a8d4089790
1f10220306 <s1f102203067@iniad.org>
2023-07-06 14:13:07 +0900

    警告を修正

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,12 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -52,13 +54,15 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
         response = client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']),0)
+        self.assertEqual(len(response.context['tasks']), 0)
+    
     def test_index_post(self):
         client = Client()
         data = {'title': 'Test Task', 'due_at': '2023-06-30 23:59:59'}
--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
+
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
@@ -14,8 +15,7 @@ def index(request):
     else:
         tasks = Task.objects.order_by('-posted_at')
 
-
     context = {
-        'tasks' : tasks
+        'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
